4c7e4e333717c8a02801eae51a0eb9e0b83dc6d1
ggaillard <gaillard42@gmail.com>
2025-01-10 16:06:30 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Utilisateur(Base):
+    __tablename__ = "utilisateurs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nom = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+
+    abonnements = relationship("Abonnement", back_populates="utilisateur")
+
+
+class Abonnement(Base):
+    __tablename__ = "abonnements"
+
+    id = Column(Integer, primary_key=True, index=True)
+    utilisateur_id = Column(Integer, ForeignKey("utilisateurs.id"))
+    service = Column(String, index=True)
+    prix = Column(Float)
+
+    utilisateur = relationship("Utilisateur", back_populates="abonnements")
